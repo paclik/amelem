74b9c264666d81502e9cc4444881056a0623db74
amelem <amelem@gmail.com>
2010-07-30 11:59:11 +0200

    ss

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100719143215) do
+ActiveRecord::Schema.define(:version => 20100730085329) do
 
   create_table "body_types", :force => true do |t|
     t.string   "type_cz"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(:version => 20100719143215) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.text     "state"
+    t.string   "adresa"
   end
 
   create_table "scripts", :force => true do |t|
