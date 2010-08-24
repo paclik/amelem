0aea40c68ba774f0e0cb0b2daccc4cc02eb77b2d
amelem <amelem@gmail.com>
2010-08-24 13:13:16 +0200

    hotovo

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -21,8 +21,21 @@ class Contact < ActiveRecord::Base
 		unless (weight == nil) or (height == nil) then
 			
 			indexBmi = (weight.to_f / ((height.to_f/100) * (height.to_f/100)) *100).round / 100.0
+		else
+			indexBmi = "no data"
 		end
+		
 		return indexBmi
 	end	
-   	
+  
+	def bfat
+		unless (weight == nil) or (height == nil) or (age == nil) or (gender == nil) then
+			if gender then @gender = 1 else @gender = 0 end
+			indexBfat = ((1.2*bmi.to_f + 0.23*age.to_f - 10.8 *@gender.to_f - 5.4)*100).round / 100.0
+		else
+			indexBfat = "no data"
+		end
+		return indexBfat
+	end	
+ 	
 end
