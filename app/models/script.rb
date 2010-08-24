ccb8cd79b5dd6c448f86c6d25da15519d2e3693d
amelem <amelem@gmail.com>
2010-08-24 12:44:06 +0200

    hotovo

--- app/models/script.rb
+++ app/models/script.rb
@@ -6,18 +6,15 @@ class Script < ActiveRecord::Base
 		$i = 0
 		nameAbbr3 = ""
 		#1b. Pozvání do poradny - z dotazniku
-		#1b.PozDoPor-ZDot
+		#1b.PozDoPorZDot
 		while (name.gsub(/^(.*?\s){#{$i}}(.*?)$/,'\1') != name) 
 			nameAbbr3 = nameAbbr3 +  name.gsub(/^(.*?\s){#{$i}}(.*?)$/,'\1').capitalize.mb_chars.normalize(:kd).gsub(/[^x00-\x7F]/n, '').to_s[0..2]
 			$i =  $i + 1
 		end
 		$i =  $i - 1
 		nameAbbr3 = nameAbbr3 + name.gsub(/^(.*?\s){#{$i}}(.*?)$/,'\2').capitalize.mb_chars.normalize(:kd).gsub(/[^x00-\x7F]/n, '').to_s[0..2]
-		
 		nameAbbr= nameAbbr3.gsub(/\s/,"")
 	
 	end
-  
-			
-	
+
 end
